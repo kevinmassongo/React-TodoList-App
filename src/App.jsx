8b7dc1d133e5a8f76bc2a0d15af6802298ba7ec2
Kevin Massongo <kevinmassongo196@gmail.com>
2023-11-30 14:00:06 +0100

    I initializ my project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //state (état)
+
+  //comportement
+
+  //rendu
+  return (
+    <>
+      <div>
+        <ul>
+          <li>Abricot</li>
+          <li>Banana</li>
+          <li>Cerise</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
