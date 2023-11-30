95d944adfae59403cb2c6510f0918f2f87d0210c
Kevin Massongo <kevinmassongo196@gmail.com>
2023-11-30 16:42:55 +0100

    add codes for deleting a fruit in list

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,41 @@ import './App.css'
 
 function App() {
   //state (état)
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Abricot"},
+    { id: 2, nom: "Banane"},
+    { id: 3, nom: "Cerise"},
+  ])
+  
 
+  
   //comportement
+  const handleDelete = (id) =>{
+    console.log(id);
+
+    //copie du state
+    const fruitsCopy = fruits.slice()
+    console.log(fruitsCopy);
+
+    //manipuler mon state
+    const fruitsCopyUpdated = fruits.filter((fruit)=> fruit.id !== id)
+    console.log(fruitsCopyUpdated);
+
+    //modifier mon state avec le setter
+    setFruits(fruitsCopyUpdated)
+  }
 
   //rendu
   return (
     <>
       <div>
+        <h1>Liste des fruits</h1>
         <ul>
-          <li>Abricot</li>
-          <li>Banana</li>
-          <li>Cerise</li>
+          {fruits.map((fruit)=>
+            <li key={fruit.id}>
+              {fruit.nom}<button onClick={()=>handleDelete(fruit.id)}>X</button>
+            </li> 
+          )}
         </ul>
       </div>
     </>
